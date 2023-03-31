4f1e77f036e8f2bdf7fd6a235083332326c8c019
Carolanne Yvon <elolithia@gmail.com>
2023-03-31 10:52:20 +0200

    Structure du projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TodosComponent } from './todos/todos.component';
+import { TodoListComponent } from './todos/todo-list/todo-list.component';
+import { TodoDetailsComponent } from './todos/todo-details/todo-details.component';
+import { TodoFormComponent } from './todos/todo-form/todo-form.component';
+import { UsersComponent } from './users/users.component';
+import { CategoryFilterComponent } from './category-filter/category-filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TodosComponent,
+    TodoListComponent,
+    TodoDetailsComponent,
+    TodoFormComponent,
+    UsersComponent,
+    CategoryFilterComponent
   ],
   imports: [
     BrowserModule,
